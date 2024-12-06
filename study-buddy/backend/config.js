021205f29725f2be1738d7f922c802aafa5c1791
Matthew Castillo <mtc325@lehigh.edu>
2024-12-06 00:57:12 -0500

    dotenv and firebase credentials

--- study-buddy/backend/config.js
+++ study-buddy/backend/config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -8,14 +10,14 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBvR_JRck0iJwo-futKeJTPYhi-2g1OuSU",
-  authDomain: "kms-6287f.firebaseapp.com",
-  databaseURL: "https://kms-6287f-default-rtdb.firebaseio.com",
-  projectId: "kms-6287f",
-  storageBucket: "kms-6287f.firebasestorage.app",
-  messagingSenderId: "716058038214",
-  appId: "1:716058038214:web:d259ae3cc198f3f9faacc7",
-  measurementId: "G-JCRMQ31H7Z"
+  apiKey: process.env.FIREBASE_APIKEY, //"AIzaSyBvR_JRck0iJwo-futKeJTPYhi-2g1OuSU",
+  authDomain: process.env.FIREBASE_AUTHDOMAIN, //"kms-6287f.firebaseapp.com",
+  databaseURL: process.env.FIREBASE_DATABASEURL, //"https://kms-6287f-default-rtdb.firebaseio.com",
+  projectId: process.env.FIREBASE_PROJECTID, //"kms-6287f",
+  storageBucket: process.env.SOTRAGEBUCKET, //"kms-6287f.firebasestorage.app",
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID, //"716058038214",
+  appId: process.env.FIREBASE_APPID, //"1:716058038214:web:d259ae3cc198f3f9faacc7",
+  measurementId: process.env.FIREBASE_MEASUREMENTID //"G-JCRMQ31H7Z"
 };
 
 // Initialize Firebase
